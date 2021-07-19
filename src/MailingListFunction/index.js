134728759db687124dff69ca7c400a4ab08f4553
Danielle H <danielle.heberling@koan.co>
2021-07-19 11:53:13 -0700

    Add more info to functions

--- src/MailingListFunction/index.js
+++ src/MailingListFunction/index.js
@@ -2,5 +2,7 @@ exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
+  // Add logic here to add/edit the user's mailing list subscriptions
+
   return {};
 };
